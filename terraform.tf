979091271984c6873e7643ed1b9fc00242fd8f15
bklevels <byron@chargebackchampion.com>
2018-05-18 19:12:43 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,9 +11,9 @@ resource "aws_instance" "sampleInstances" {
   #The AMI specified above is a public Ubuntu image, backed by EBS. It was available on AWS at the time of
   #authoring this sample. You can also specify your own AMI image or use any other public EBS backed image if the
   #AMI used in the sample is decommissioned.
-  availability_zone = "us-east-1b"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
+  availability_zone = "us-west-2a"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
   instance_type = "t2.micro"
-  subnet_id = "subnet-5bfa2e76"  #REPLACE THIS WITH YOUR SUBNET ID
+  subnet_id = "subnet-44da8723"  #REPLACE THIS WITH YOUR SUBNET ID
   associate_public_ip_address = true
   source_dest_check = false
 
